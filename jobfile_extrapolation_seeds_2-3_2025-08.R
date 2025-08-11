853a5b3cae3564fc23e076eb57a075def5a6902f
Emily Cantrell <e.cantrell@princeton.edu>
2025-08-11 14:32:01 -0700

    New jobfiles for data matrix paper

---
+++ jobfile_extrapolation_seeds_2-3_2025-08.R
@@ -0,0 +1,105 @@
+# This is a configuration file specifying settings for parallelizaton, feature
+# choices, train-test splits, modeling options, and results output
+
+# Set up
+library(tidyverse)
+
+# Parallelization settings
+seed_job <- 0
+seed_worker <- 0
+workers_grid_row <- 3
+workers_metric_for_selecting_pipelines <- 3
+#n_thread_within_worker <- -1
+
+# Feature choices
+# NB: make sure to use equal signs here, not arrows
+feature_set_settings <- list(
+  without_leakage = c("GBAPERSOONTAB", "GBAHUISHOUDENSBUS_without_leakage", "prefer_official_train", "FAMILIENETWERKTAB", "live_in_partner"),
+  prefer_official_train = c("prefer_official_train"),
+  AS_FS = c("family_age_and_sex_from_prefer_submission", "family_structure"),
+  ego_AS = c("ego_AS")
+)
+
+# Train-test splits 
+sampling_files <- c("dms_samples_seed_2_subsamples_1",
+                    "dms_samples_seed_2_subsamples_2",
+                    "dms_samples_seed_2_subsamples_3",
+                    "dms_samples_seed_2_subsamples_4",
+                    "dms_samples_seed_2_subsamples_5",
+                    "dms_samples_seed_3_subsamples_1",
+                    "dms_samples_seed_3_subsamples_2",
+                    "dms_samples_seed_3_subsamples_3",
+                    "dms_samples_seed_3_subsamples_4",
+                    "dms_samples_seed_3_subsamples_5")
+data_splits <- bind_rows(
+  expand_grid(
+    training_sets = c("train_sample_n_100",
+                      "train_sample_n_200",
+                      "train_sample_n_300",
+                      "train_sample_n_400",
+                      "train_sample_n_500",
+                      "train_sample_n_600",
+                      "train_sample_n_700",
+                      "train_sample_n_800",
+                      "train_sample_n_900",
+                      "train_sample_n_1000",
+                      "train_sample_n_2000",
+                      "train_sample_n_3000",
+                      "train_sample_n_4000",
+                      "train_sample_n_5000",
+                      "train_sample_n_6000",
+                      "train_sample_n_7000",
+                      "train_sample_n_8000",
+                      "train_sample_n_9000",
+                      "train_sample_n_10000"),
+    selection_sets = c("evaluation_selection_50_percent_split"), # Evaluation sets we use to select the best pipelines
+    test_sets = c("evaluation_test_50_percent_split", "official_holdout_set") # Evaluation sets we use for holdout evaluations.
+  )
+)
+
+# Modeling options
+model_settings <- list(
+  # These are default catboost settings, and these are the only model-
+  # hyperparameter combination we have time to explore, but our code supports
+  # tuning other models and hyperparameter options, as commented out below
+  catboost = list(tibble(grid_row = pmap(expand_grid(
+    learning_rate = c(NA),
+    subsample = c(.8),
+    depth = c(6)
+  ),
+  list)),
+  steps = c(1000)) # how many trees in catboost?
+  # catboost = list(tibble(grid_row = pmap(expand_grid(
+  #   learning_rate = c(.009, .03, .09, .3, .9, NA),
+  #   subsample = c(.2, .5, .8, 1),
+  #   depth = c(1, 2, 4, 6, 8, 10)
+  # ),
+  # list)),
+  # steps = c(200, 400, 600, 800, 1000)),
+  # xgboost = list(tibble(grid_row = pmap(expand_grid(
+  #   eta = c(.009, .03, .09, .3, .9),
+  #   subsample = c(.2, .5, .8, 1),
+  #   max_depth = c(1, 2, 4, 6, 8, 10)
+  # ),
+  # list)),
+  # steps = c(200, 400, 600, 800, 1000)),
+  # elastic_net = list(tibble(grid_row = pmap(bind_rows(expand_grid(
+  #   alpha = c(0, .15, .3, .5, .7, .85, 1),
+  #   lambda = c(NA)
+  # ),
+  # tibble(alpha = 1, lambda = 0)),
+  # list)),
+  # steps = c(1, 2, 3, 4, 5))
+  )
+n_grid_row <- 1 # how many hyperparameter combinations to sample from expanded
+# grid?
+
+# Performance metrics
+metrics_for_all_pipelines <- c("LogLoss", "MSE", "In_Sample_R2", "R2_Holdout", "AUC") # Deciles_for_Calibration
+metrics_for_selecting_pipelines <- c("LogLoss")
+metrics_for_winning_pipelines <- c("F1_Score", "Accuracy") # F1_Score
+threshold_increment <- .01
+n_bootstrap <- 2000 
+
+save_only_winning_hyperparameter_draw_results <- FALSE
+results_path <- "results_extrapolation_seeds_2-3_2025-08.csv"
